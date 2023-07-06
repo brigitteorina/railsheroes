241e5fa9f8633352e27819e0208e7ff3ee1e83cd
Brigitte <brigitteorina@gmail.com>
2023-07-06 07:09:48 +0300

    trial data

--- app/models/power.rb
+++ app/models/power.rb
@@ -3,6 +3,5 @@ class Power < ApplicationRecord
     has_many :heroes, through: :hero_powers
 
     validates :description, presence: true, length: { minimum: 20 }
-    validates :strength, inclusion: { in: ['Strong', 'Weak', 'Average'] }
-    validates :name, presence: true
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Hero.create(name: "marvellous", super_name: "Ms. Marvel")
+Hero.create(name: "Phoebe", super_name: "Squirrel Girl")
+Hero.create(name: "Stacey", super_name: "Spider Woman")
+
+Power.create(name: "Super Strength", description: "Gives super-human strengths")
+Power.create(name: "Flight", description: "Gives the ability to fly through the skies at a high speed")
+
