fb62694951d555c911a8388dfd82a136158a3601
Brigitte <brigitteorina@gmail.com>
2023-07-06 07:21:14 +0300

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'hero_powers/create'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
 end
