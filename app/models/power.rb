20b236d31e1bfb005f1bc5605b4306acd8260eba
Brigitte <brigitteorina@gmail.com>
2023-07-06 06:49:17 +0300

    power validations

--- app/models/power.rb
+++ app/models/power.rb
@@ -3,4 +3,6 @@ class Power < ApplicationRecord
     has_many :heroes, through: :hero_powers
 
     validates :description, presence: true, length: { minimum: 20 }
+    validates :strength, inclusion: { in: ['Strong', 'Weak', 'Average'] }
+    validates :name, presence: true
 end
