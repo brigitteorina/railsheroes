47277c136821ec380d00cf54e280c4844106d083
Brigitte <brigitteorina@gmail.com>
2023-07-06 07:31:47 +0300

    edit/update a given power

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -12,4 +12,17 @@ class PowersController < ApplicationController
           render json: { error: "Power not found" }, status: :not_found
         end
       end
+
+      def update
+        power = Power.find_by(id: params[:id])
+        if power
+          if power.update(power_params)
+            render json: power
+          else
+            render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: "Power not found" }, status: :not_found
+        end
+      end
 end
